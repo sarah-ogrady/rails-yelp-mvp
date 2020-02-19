b6321a5bcbf3b89328c7357d51c5e4f29ac83f65
Sarah OGrady <sarahfrancesogrady@gmail.com>
2020-02-19 22:06:09 +0000

    added drop downs and link backs

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.destroy_all
+puts 'Creating 100 fake restaurants...'
+10.times do
+  restaurant = Restaurant.new(
+    name: Faker::Company.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: %w[chinese italian japanese french belgian].sample
+  )
+  restaurant.save!
+end
+puts 'Finished!'
